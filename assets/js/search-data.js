eaf835059fd5769f86616576a1afae5e81bb4f56
chriskcovert <chris.covert@outlook.com>
2025-07-25 17:04:23 +0000

    Deploying to gh-pages from @ chriskcovert/chriskcovert.github.io@16d5b3edd4909585d63b0be8272eebb0f02844fc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -88,6 +88,11 @@ ninja.data = [{
           description: "Building engagement on a Discord Server dedicated to the profession of project management",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
+            },},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project/";
             },},{
         id: 'social-email',
         title: 'email',
